6b86185bb68f019ed789b72d578602ae52e4df1f
fikre <fikre8186@gmail.com>
2025-07-26 11:24:47 +0300

    header bootsrap Toggler

--- apple-clone/src/Conponents/Header/Header.jsx
+++ apple-clone/src/Conponents/Header/Header.jsx
@@ -1,37 +1,34 @@
-import React,{useEffect} from "react";
+import React from "react";
 import Cart from "../../assets/images/cart.png";
 import SearchIcon from "../../assets/images/search-icon.png";
 import Logo from "../../assets/images/logo.png";
-import $ from "jquery";
 
 function Header() {
-  useEffect(() => {
-    const $naves = $(".navbar-collapse");
-    const $toggler = $(".toggler");
-
-    $toggler.on("click", function () {
-      $(this).toggleClass("fa-xmark");
-      $naves.toggleClass("active");
-    });
-
-    // Cleanup to avoid memory leaks
-    return () => {
-      $toggler.off("click");
-    };
-  }, []);
-
   return (
     <>
       <header className="header">
         <div className="container-md header-2">
-          <nav className="navbar navbar-expand-md navbar-light">
+          <nav className="navbar navbar-expand-md navbar-light w-100">
             <a className="navbar-brand" href="#">
-              <img src={Logo}className="apple-logo" />
+              <img src={Logo} className="apple-logo" alt="Apple Logo" />
             </a>
 
-            {/* <!-- Collapsible nav items --> */}
+            {/* ✅ Bootstrap Toggler Button */}
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            {/* ✅ Collapsible nav items */}
             <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
+              <ul className="navbar-nav mx-auto">
                 <li className="nav-item">
                   <a className="nav-link" href="#">
                     Store
@@ -88,23 +85,21 @@ function Header() {
                   </a>
                 </li>
               </ul>
+              {/* ✅ Icons inside navbar for responsive right align */}
+              <div className="icons d-flex ms-auto">
+                <a className="nav-icon" href="#">
+                  <img src={SearchIcon} alt="Search" />
+                </a>
+                <a className="nav-icon cartt" href="#">
+                  <img src={Cart} alt="Cart" />
+                </a>
+              </div>
             </div>
           </nav>
-          <div className="icons">
-            <a className="nav-icon" href="#">
-              <img src={SearchIcon} alt="Search" />
-            </a>
-            <a className="nav-icon cartt " href="#">
-              <img src={Cart} alt="Cart" />
-            </a>
-            <a href="#">
-              <i className="fa-solid fa-bars toggler"></i>
-            </a>
-          </div>
         </div>
       </header>
-      
     </>
   );
 }
+
 export default Header;
